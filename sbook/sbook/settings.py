7ddc06c52c2d6edaeeb607a0de5c47d908200ab6
Yash Birajdar <birajdaryash01@gmail.com>
2024-01-11 00:04:46 +0530

    Update settings.py

--- sbook/sbook/settings.py
+++ sbook/sbook/settings.py
@@ -174,7 +174,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'birajdaryash01@gmail.com'
-EMAIL_HOST_PASSWORD = "prsr mlfm qoof jhgh"
+EMAIL_HOST_PASSWORD = "zzzz zzzz zzzz zzzz"
 
 DJOSER = {
     'LOGIN_FIELD' : 'email',
